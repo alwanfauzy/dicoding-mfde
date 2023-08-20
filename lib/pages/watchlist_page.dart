79471ac4ece1453376b6f549ebe7060b2805d86d
alwanfauzy <alwanfauzi13@gmail.com>
2023-08-20 20:51:14 +0700

    modularize tv and movie

---
+++ lib/pages/watchlist_page.dart
@@ -0,0 +1,36 @@
+import 'package:tv/pages/watchlist_Tv_page.dart';
+import 'package:movie/pages/watchlist_movies_page.dart';
+import 'package:flutter/material.dart';
+
+class WatchlistPage extends StatefulWidget {
+  static const ROUTE_NAME = '/watchlist';
+
+  const WatchlistPage({Key? key}) : super(key: key);
+
+  @override
+  State<WatchlistPage> createState() => _WatchlistPageState();
+}
+
+class _WatchlistPageState extends State<WatchlistPage> {
+  final List<Tab> _tabs = <Tab>[
+    const Tab(text: 'Movies'),
+    const Tab(text: 'TV'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: _tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Watchlist'),
+          bottom: TabBar(tabs: _tabs),
+        ),
+        body: TabBarView(children: [
+          WatchlistMoviesPage(),
+          WatchlistTvPage(),
+        ]),
+      ),
+    );
+  }
+}
---
+++ tv/test/dummy_data/dummy_objects.dart
@@ -0,0 +1,68 @@
+import 'package:core/data/models/tv_table.dart';
+import 'package:core/domain/entities/genre.dart';
+import 'package:core/domain/entities/tv.dart';
+import 'package:core/domain/entities/tv_detail.dart';
+import 'package:core/domain/entities/tv_season.dart';
+
+final testTv = Tv(
+  backdropPath: '/muth4OYamXf41G2evdrLEg8d3om.jpg',
+  genreIds: [14, 28],
+  id: 557,
+  originalName: 'Spider-Man',
+  overview:
+      'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
+  popularity: 60.441,
+  posterPath: '/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
+  name: 'Spider-Man',
+  voteAverage: 7.2,
+  voteCount: 13507,
+);
+
+final testTvList = [testTv];
+
+final testTvDetail = TvDetail(
+  adult: false,
+  backdropPath: 'backdropPath',
+  genres: [Genre(id: 1, name: 'Action')],
+  id: 1,
+  originalName: 'originalName',
+  overview: 'overview',
+  posterPath: 'posterPath',
+  runtime: 120,
+  name: 'name',
+  voteAverage: 1,
+  voteCount: 1,
+  numberOfSeasons: 1,
+  numberOfEpisodes: 12,
+  seasons: [
+    TvSeason(
+      id: 0,
+      airDate: "airDate",
+      name: "name",
+      posterPath: "posterPath",
+      seasonNumber: 1,
+      episodeCount: 12,
+    )
+  ],
+);
+
+final testWatchlistTv = Tv.watchlist(
+  id: 1,
+  name: 'name',
+  posterPath: 'posterPath',
+  overview: 'overview',
+);
+
+final testTvTable = TvTable(
+  id: 1,
+  name: 'name',
+  posterPath: 'posterPath',
+  overview: 'overview',
+);
+
+final testTvMap = {
+  'id': 1,
+  'overview': 'overview',
+  'posterPath': 'posterPath',
+  'name': 'name',
+};
